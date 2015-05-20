27dda4d2d08be542822cd16822549f1a38516247
syota-itakura <syota.itakura@gu3.co.jp>
2015-05-20 18:50:52 +0900

    photoページでwindow幅に応じて動作を変える

--- static/js/photos.js
+++ static/js/photos.js
@@ -4,12 +4,17 @@
 		this.element = element;
 		this.src = element.src;
 		this.open = function(){
-			TINY.box.show({
-				image: self.src,
-				boxid: 'frameless',
-				animate: true,
-				openjs: function(){}
-			});
+			if(document.documentElement.clientWidth >= 768){
+				TINY.box.show({
+					image: self.src,
+					boxid: 'frameless',
+					animate: true,
+					openjs: function(){}
+				});
+			}else{
+//				global.open(self.src);
+				global.location.href = self.src;
+			}
 		};
 		this.element.onclick = function(){
 			self.open();
@@ -20,7 +25,7 @@
 		var imageCardElements = document.getElementsByClassName("img-card");
 		var len = imageCardElements.length;
 		var i = 0;
-		var imageCard = null;
+		var _imageCard = null;
 		for(i=0; i<len; i++){
 			_imageCard = new ImageCard(imageCardElements[i]);
 		}
