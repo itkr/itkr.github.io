dbb19bb623999bcf3e4f017072c34b01e047b962
syota-itakura <syota.itakura@gu3.co.jp>
2015-05-21 18:42:51 +0900

    仮データ更新

--- static/js/sports.js
+++ static/js/sports.js
@@ -27,7 +27,7 @@ var myDoughnut = new Chart(
 */
 
 var data = {
-    labels: ["-12(年)", "-11", "-10", "-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "現在"],
+    labels: ["2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"],
     datasets: [
         {
             label: "My First dataset",
@@ -51,6 +51,38 @@ var data = {
 //        }
     ]
 };
-new Chart(document.getElementById("sample").getContext("2d")).Line(data, {
-    bezierCurve: false
-});
+
+var option = {
+    ///Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines : true,
+    //String - Colour of the grid lines
+    scaleGridLineColor : "rgba(0,0,0,.05)",
+    //Number - Width of the grid lines
+    scaleGridLineWidth : 1,
+    //Boolean - Whether to show horizontal lines (except X axis)
+    scaleShowHorizontalLines: true,
+    //Boolean - Whether to show vertical lines (except Y axis)
+    scaleShowVerticalLines: true,
+    //Boolean - Whether the line is curved between points
+    bezierCurve : true,
+    //Number - Tension of the bezier curve between points
+    bezierCurveTension : 0.4,
+    //Boolean - Whether to show a dot for each point
+    pointDot : true,
+    //Number - Radius of each point dot in pixels
+    pointDotRadius : 4,
+    //Number - Pixel width of point dot stroke
+    pointDotStrokeWidth : 1,
+    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+    pointHitDetectionRadius : 20,
+    //Boolean - Whether to show a stroke for datasets
+    datasetStroke : true,
+    //Number - Pixel width of dataset stroke
+    datasetStrokeWidth : 2,
+    //Boolean - Whether to fill the dataset with a colour
+    datasetFill : true,
+    //String - A legend template
+//    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+};
+
+new Chart(document.getElementById("sample").getContext("2d")).Line(data, option);
