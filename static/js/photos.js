db7927f0e640f8e0e34895f0252867cfe5774bb6
syota-itakura <syota.itakura@gu3.co.jp>
2015-05-21 10:33:00 +0900

    photos.jsをオプションありでjslintざっくり対応

--- static/js/photos.js
+++ static/js/photos.js
@@ -1,34 +1,34 @@
-(function(global, document) {
-	var ImageCard = function(element){
-		var self = this;
-		this.element = element;
-		this.src = element.src;
-		this.open = function(){
-			if(document.documentElement.clientWidth >= 768){
-				TINY.box.show({
-					image: self.src,
-					boxid: 'frameless',
-					animate: true,
-					openjs: function(){}
-				});
-			}else{
-//				global.open(self.src);
-				global.location.href = self.src;
-			}
-		};
-		this.element.onclick = function(){
-			self.open();
-		};
-	};
+(function (global, document) {
+    'use strict';
+    var ImageCard = function (element) {
+        var self = this;
+        this.element = element;
+        this.src = element.src;
+        this.open = function () {
+            if (document.documentElement.clientWidth >= 768) {
+                TINY.box.show({
+                    image: self.src,
+                    boxid: 'frameless',
+                    animate: true
+                    // openjs: function () {}
+                });
+            } else {
+                global.location.href = self.src;
+            }
+        };
+        this.element.onclick = function () {
+            self.open();
+        };
+    };
 
-	function init(){
-		var imageCardElements = document.getElementsByClassName("img-card");
-		var len = imageCardElements.length;
-		var i = 0;
-		var _imageCard = null;
-		for(i=0; i<len; i++){
-			_imageCard = new ImageCard(imageCardElements[i]);
-		}
-	}
-	global.onload = init;
-})(this, this.document);
+    function init() {
+        var imageCardElements = document.getElementsByClassName("img-card"),
+            len = imageCardElements.length,
+            i = 0,
+            _imageCard = null;
+        for (i = 0; i < len; i++) {
+            _imageCard = new ImageCard(imageCardElements[i]);
+        }
+    }
+    global.onload = init;
+}(this, this.document));
