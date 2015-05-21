5da0cb22f77f3093c3258fd9c8dd7a4caf6cc7b0
itkr <itkrst@gmail.com>
2015-05-22 00:24:51 +0900

    Sportsページなど更新

--- static/js/sports.js
+++ static/js/sports.js
@@ -26,19 +26,23 @@ var myDoughnut = new Chart(
     document.getElementById("sample").getContext("2d")).Doughnut(doughnutData);
 */
 
-var data = {
-    labels: ["2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [55, 60, 61, 62, 55, 55, 60, 60, 60, 62, 67, 62, 60]
-        }
+(function(global, document){
+	var $ = function(id){
+		return document.getElementById(id);
+	};
+	var data = {
+	    labels: ["2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"],
+	    datasets: [
+	        {
+	            label: "My First dataset",
+	            fillColor: "rgba(220,220,220,0.2)",
+	            strokeColor: "rgba(220,220,220,1)",
+	            pointColor: "rgba(220,220,220,1)",
+	            pointStrokeColor: "#fff",
+	            pointHighlightFill: "#fff",
+	            pointHighlightStroke: "rgba(220,220,220,1)",
+	            data: [55, 60, 61, 62, 55, 55, 60, 60, 60, 62, 65, 62, 60]
+	        }
 //        {
 //            label: "My Second dataset",
 //            fillColor: "rgba(151,187,205,0.2)",
@@ -47,42 +51,10 @@ var data = {
 //            pointStrokeColor: "#fff",
 //            pointHighlightFill: "#fff",
 //            pointHighlightStroke: "rgba(151,187,205,1)",
-//            data: [55, 60, 61, 62, 55, 55, 60, 60, 60, 62, 67, 62, 60]
+//            data: [55, 60, 61, 62, 55, 55, 60, 60, 60, 62, 65, 62, 60]
 //        }
-    ]
-};
-
-var option = {
-    ///Boolean - Whether grid lines are shown across the chart
-    scaleShowGridLines : true,
-    //String - Colour of the grid lines
-    scaleGridLineColor : "rgba(0,0,0,.05)",
-    //Number - Width of the grid lines
-    scaleGridLineWidth : 1,
-    //Boolean - Whether to show horizontal lines (except X axis)
-    scaleShowHorizontalLines: true,
-    //Boolean - Whether to show vertical lines (except Y axis)
-    scaleShowVerticalLines: true,
-    //Boolean - Whether the line is curved between points
-    bezierCurve : true,
-    //Number - Tension of the bezier curve between points
-    bezierCurveTension : 0.4,
-    //Boolean - Whether to show a dot for each point
-    pointDot : true,
-    //Number - Radius of each point dot in pixels
-    pointDotRadius : 4,
-    //Number - Pixel width of point dot stroke
-    pointDotStrokeWidth : 1,
-    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-    pointHitDetectionRadius : 20,
-    //Boolean - Whether to show a stroke for datasets
-    datasetStroke : true,
-    //Number - Pixel width of dataset stroke
-    datasetStrokeWidth : 2,
-    //Boolean - Whether to fill the dataset with a colour
-    datasetFill : true,
-    //String - A legend template
-//    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-};
-
-new Chart(document.getElementById("sample").getContext("2d")).Line(data, option);
+	    ]
+	};
+	var option = {};
+	new Chart($("graph-weight").getContext("2d")).Line(data, option);
+}(this, this.document));
