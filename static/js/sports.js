6b630bdf86a9898b59d2d8e71340850b89ddab37
syota-itakura <syota.itakura@gu3.co.jp>
2015-05-22 19:17:48 +0900

    グラフなど更新

--- static/js/sports.js
+++ static/js/sports.js
@@ -1,60 +1,29 @@
-/*
-var doughnutData = [
-　　{
-　　　value: 30,
-　　　color:"#aaf2fb"
-　　},
-　　{
-　　　value: 50,
-　　　color: "#ffb6b9"
-　　},
-　　{
-　　　value: 120,
-　　　color: "#ffe361"
-　　},
-   {
-　　　value: 170,
-　　　color: "#fbaa6e"
-　　},
-   {
-　　　value: 70,
-　　　color: "#A8BECB"
-　　}
-];
- 
-var myDoughnut = new Chart(
-    document.getElementById("sample").getContext("2d")).Doughnut(doughnutData);
-*/
-
 (function(global, document){
-	var $ = function(id){
-		return document.getElementById(id);
-	};
-	var data = {
-	    labels: ["2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"],
-	    datasets: [
-	        {
-	            label: "My First dataset",
-	            fillColor: "rgba(220,220,220,0.2)",
-	            strokeColor: "rgba(220,220,220,1)",
-	            pointColor: "rgba(220,220,220,1)",
-	            pointStrokeColor: "#fff",
-	            pointHighlightFill: "#fff",
-	            pointHighlightStroke: "rgba(220,220,220,1)",
-	            data: [55, 60, 61, 62, 55, 55, 60, 60, 60, 62, 65, 62, 60]
-	        }
-//        {
-//            label: "My Second dataset",
-//            fillColor: "rgba(151,187,205,0.2)",
-//            strokeColor: "rgba(151,187,205,1)",
-//            pointColor: "rgba(151,187,205,1)",
-//            pointStrokeColor: "#fff",
-//            pointHighlightFill: "#fff",
-//            pointHighlightStroke: "rgba(151,187,205,1)",
-//            data: [55, 60, 61, 62, 55, 55, 60, 60, 60, 62, 65, 62, 60]
-//        }
-	    ]
-	};
-	var option = {};
-	new Chart($("graph-weight").getContext("2d")).Line(data, option);
+    var $ = function(id){
+        return document.getElementById(id);
+    };
+
+	// weight
+    new Chart($("graph-weight").getContext("2d")).Line({
+        labels: ["2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"],
+        datasets: [{
+            label: "Weight",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [55, 60, 61, 62, 55, 55, 60, 60, 60, 62, 65, 62, 60]
+        }]
+    }, {});
+
+	// events
+	new Chart($("graph-events").getContext("2d")).Doughnut([
+        {label: 'Ice hockey', value: 15, color:"#aaf2fb"},
+        {label: 'Runnnig', value: 1, color: "#ffb6b9"},
+        {label: 'Climbing', value: 1, color: "#ffe361"},
+        {label: 'Weight', value: 2, color: "#fbaa6e"},
+        {label: 'Other', value: 0, color: "#A8BECB"}
+    ]);
 }(this, this.document));
