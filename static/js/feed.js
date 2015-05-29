3a363504436f0fe9ba2bffa42d30fc7a01ca6977
syota-itakura <syota.itakura@gu3.co.jp>
2015-05-29 12:27:26 +0900

    Feedをトップに表示する

--- static/js/feed.js
+++ static/js/feed.js
@@ -1,9 +1,29 @@
-(function (global, document) {
-    'use strict';
+(function(global, document) {
 
     var NUM_ENTRIES = 3,
         URL = "http://qiita.com/itkr/feed",
         OUTPUT_ID = "feed";
+
+    var baseHTML = '<div class="feed">'
+	+ '  <div class="pure-g">'
+	+ '    <div class="pure-u-1 pure-u-md-1-4 section-part feed__image">'
+	+ '      <a href="{link}" target="_blank"><img src="{img}"></a>'
+	+ '    </div>'
+	+ '    <div class="pure-u-1 pure-u-md-3-4 section-part feed__contents">'
+	+ '      <h4><a href="{link}" target="_blank">{title}</a></h4>'
+	+ '      <div class="feed__summary">{summary}</div>'
+	+ '      <div class="feed__data">({date})</div>'
+	+ '    </div>'
+	+ '  </div>'
+	+ '</div>';
+
+    var format = function(string, dict) {
+        var string = string.replace();
+        for (key in dict) {
+            string = string.split('{' + key + '}').join(dict[key]);
+        }
+        return string;
+    };
 
     function formatDate(date) {
         var year = date.getFullYear(),
@@ -13,8 +33,16 @@
     }
 
     function makeEntryHTML(entry) {
-        var date = formatDate(new Date(entry.publishedDate));
-        return '<li>(' + date + ') <a href="' + entry.link + '">' + entry.title + '</a></li>';
+        var date = formatDate(new Date(entry.publishedDate)),
+            img = entry.content.match(/src="(.*?)"/igm);
+        img = img === null ? 'static/img/thumbnail/miku.png' : img[0].replace('src=', '').split('"').join('');
+        return format(baseHTML, {
+            'img': img,
+            'title': entry.title,
+            'date': date,
+            'link': entry.link,
+            'summary': entry.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').substring(0, 100) + '...'
+        })
     }
 
     function init() {
@@ -22,9 +50,9 @@
             html = "",
             i = 0;
         feed.setNumEntries(NUM_ENTRIES);
-        feed.load(function (result) {
+        feed.load(function(result) {
             if (result.error) {
-                // console.log(result.error.code + ":" + result.error.message);
+                console.log(result.error.code + ":" + result.error.message);
                 return;
             }
             for (i = 0; i < result.feed.entries.length; i++) {
